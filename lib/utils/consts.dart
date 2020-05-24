1ea0c7eba13e69c9fb6320d6f266fde695d71efe
Munashe Shonge <munasheshonge@gmail.com>
2020-05-25 00:54:13 +0200

    git commit

---
+++ lib/utils/consts.dart
@@ -0,0 +1,80 @@
+import 'package:veriauth_onetruth/utils/hexcolor.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+class Constants{
+
+  //App related strings
+  static String appName = "BigglesBooks";
+
+
+  //Colors for theme
+  static Color lightPrimary = Colors.white;
+  static Color darkPrimary = Colors.black;
+  static Color lightAccent = HexColor('#AA6F73');
+  static Color darkAccent = HexColor('#AA6F73');
+  static Color lightBG = Colors.white;
+  static Color darkBG = Colors.black;
+
+  static ThemeData lightTheme = ThemeData(
+    fontFamily: "Ubuntu",
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    accentColor:  lightAccent,
+    cursorColor: lightAccent,
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          fontFamily: "Ubuntu",
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    fontFamily: "Ubuntu",
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    accentColor: darkAccent,
+    scaffoldBackgroundColor: darkBG,
+    cursorColor: darkAccent,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          fontFamily: "Ubuntu",
+          color: lightBG,
+          fontSize: 20,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+
+
+  static List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+
+    return result;
+  }
+
+  static formatBytes(bytes, decimals) {
+    if(bytes == 0) return 0.0;
+    var k = 1024,
+        dm = decimals <= 0 ? 0 : decimals,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = (log(bytes) / log(k)).floor();
+    return (((bytes / pow(k, i)).toStringAsFixed(dm)) + ' ' + sizes[i]);
+  }
+
+}
